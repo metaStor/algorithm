98a196e928c7e117833af795ce70c24a89417dac
shenhao <1427662743@qq.com>
2019-03-20 18:53:04 +0800

    丑数1,2 超级丑数， 快乐数

---
+++ src/algorithm_practices/快乐数.java
@@ -0,0 +1,57 @@
+package algorithm_practices;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * 写一个算法来判断一个数是不是"快乐数"。
+ * 一个数是不是快乐是这么定义的：对于一个正整数，
+ * 每一次将该数替换为他每个位置上的数字的平方和，然后重复这个过程直到这个数变为1，
+ * 或是无限循环但始终变不到1。如果可以变为1，那么这个数就是快乐数。
+ */
+
+public class 快乐数 {
+	
+    public static boolean isHappy(int n) {
+        // write your code here
+    	Set<Integer> map = new HashSet<>();
+    	while (n != 1) {
+    		int res = getMuti(n);
+    		System.out.println(res);
+    		if (map.contains(res)) {
+				return false;
+			}
+			if (res != 1) {
+				map.add(res);
+			}
+			n = res;
+		}
+    	return true;
+    }
+    
+    public static int getMuti(int num) {
+    	int sum = 0;
+    	while (num != 0) {
+			sum += Math.pow(num % 10, 2);
+			num /= 10;
+		}
+    	return sum;
+    }
+    
+    // 找到规律： 非快乐数中的循环中必有4
+    public static boolean isHappy1(int n) {
+        // write your code here
+    	while (n != 1 && n != 4) {
+    		int res = getMuti(n);
+    		System.out.println(res);
+			n = res;
+		}
+    	return n == 1;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isHappy1(19));
+	}
+
+}
---
+++ src/algorithm_practices/超级丑数.java
@@ -0,0 +1,43 @@
+package algorithm_practices;
+
+/*
+ * 写一个程序来找第 n 个超级丑数。
+ * 超级丑数是所有的质数因子都在给定的的质数集合内的正整数。
+ * 比如给定质数集合 [2, 7, 13, 19], 那么 [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] 是前 12 个超级丑数。
+ * 1 永远都是超级丑数。
+ * 0 < k ≤ 100, 0 < n ≤ 10^6
+ * 0 < primes[i] < 1000
+ */
+public class 超级丑数 {
+	
+	// similar with 丑数2
+    public static int nthSuperUglyNumber(int n, int[] primes) {
+        // write your code here
+    	int len = primes.length;
+    	int[] ugly = new int[n];
+    	int[] index = new int[len];
+    	int count = 1;
+    	ugly[0] = 1;
+    	while (count < n) {
+			int min = Integer.MAX_VALUE;
+			// find the minimum value
+			for (int i = 0; i < len; i++) {
+				int t = ugly[index[i]] * primes[i];
+				min = (min > t) ? t : min;
+			}
+			ugly[count] = min;
+			// update
+			for (int i = 0; i < len; i++) {
+				if (ugly[count] / ugly[index[i]] == primes[i]) index[i]++; 
+			}
+			count++;
+		}
+    	return ugly[n - 1];
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(nthSuperUglyNumber(11, new int[] {2, 3, 5}));
+	}
+
+}
